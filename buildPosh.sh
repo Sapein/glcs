060ba6b77db4796197f42414347b22216b5dd35f
Sapein <chandler182@gmail.com>
2015-04-02 11:39:17 -0500

    Continued work on Portability.
    ADDED: Comments documenting what I'm doing with buildPosh.sh and packagePosh.sh
    CONTINUED: Continued work on making glc-build portable, however it is now of low importance due to it not being used by glcs.

--- buildPosh.sh
+++ buildPosh.sh
@@ -5,13 +5,16 @@
 # PREFIX: Specify where the project is installed. default is /
 #
 
+# This is simply an easy for us to refer to when the user fails to enter enough, or too many, parameters
 usage()
 {
 	echo "usage: $0 destdir [libdir]"
 	exit 1
 }
 
-building()
+# This is simply the replacement of the for loop and the arrays
+#building() takes one parameter which is the mod that it needs to build.
+building() 
 {
 	mod=$1
 	echo "Building $mod"
@@ -27,21 +30,27 @@ building()
 	make || exit 1
 	cd ../..
 }
+
+# If the user fails to enter in enough parameters, or too little, it will go back to the usage function
 if ! ( [ $# -eq 1 ] || [ $# -eq 2 ] ); then
 	usage
 fi
 
+#This is a simple way to refer to each mod that was in the array. The convention I've used is mod[three letters] generally the first three letters of the object. The exclusion is GLS, which would be glc other wise. 
 modElf="elfhacks"
-modPak="packetstream"
+modPac="packetstream"
 modGLS="../glcs"
 
+#These variables are merely the directories that will be used.
 DESTDIR=$1
 MLIBDIR=${2:-"lib"}
 GLCSDIR=$PWD
 
+#This merely exports two variables and makes them global.
 export CMAKE_INCLUDE_PATH="$GLCSDIR/elfhacks/src:$GLCSDIR/packetstream/src" 
 export CMAKE_LIBRARY_PATH="$GLCSDIR/elfhacks/build/src:$GLCSDIR/packetstream/build/src"
 
+#This is the other part of the for loop replacement, essentially I just refer to variables as the one parameter, as building() needs
 building $modElf
-building $modPak
+building $modPac
 building $modGLS
--- packagePosh.sh
+++ packagePosh.sh
@@ -5,24 +5,28 @@
 # PREFIX: Specify where the project is installed. default is /
 #
 
+#This is simply a function to allow the user to know the parameters of this script, if they add too many or not enough.
 usage()
 {
 	echo "usage is $0 destdir"
 	exit 1
 }
 
+#This is simply how get use usage() and check to make sure the parameters are correct.
 if ! [  $# -eq 1 ]; then
 	usage
 fi
 
-mods=3 
+#This is how I replace the array. I use the same naming scheme as I defined in build(Posh).sh
 modElf="elfhacks"
 modPac="packetstream"
 modGLS="../glcs"
 
+#Two variables that decide how things are done. DESTDIR is value of the location that the user specified upon running the script. GLCSDIR is the working directory.
 DESTDIR=$1
 GLCSDIR=$PWD
 
+#This is how I did the for loop in backaging, I found it easier, and a bit more effiecent, to just retype it than to use a function for this(unlike in build(Posh).sh
 echo "Installing elfhacks to $DESTDIR"
 cd $GLCSDIR/$modElf/build
 make install || exit 1
@@ -35,6 +39,7 @@ echo "Installing ../glcs to $DESTDIR"
 cd $GLCSDIR/$modGLS/build
 make install || exit 1
 
+#This essentially copies the scripts to the Destination Directory, creates their parents if they don't exist, and sets the permissions for the files. 
 install -d -m775 $DESTDIR/share/glcs/scripts
 install -m755 $GLCSDIR/scripts/capture.sh $DESTDIR/share/glcs/scripts/capture.sh
 install -m775 $GLCSDIr/scripts/pipe_ffmeg.sh $DESTDIR/share/glcs/scripts/pipe_ffmpeg.sh
